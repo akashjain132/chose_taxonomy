a23022b6ee224401dcd0821be3fb1f85945b1edc
akash jain <akash.jain@innoraft.com>
2014-07-21 10:29:37 +0530

    Made some to made module configure

--- js/chose_taxonomy.js
+++ js/chose_taxonomy.js
@@ -1,17 +1,36 @@
 (function (jQuery) {
-Drupal.behaviors.taxonomy = {
-    attach: function (context, settings) {
-    jQuery(document).ready(function(){
-    jQuery('.field-widget-chose-taxonomy input').each(function() {
-      if (jQuery(this).is(':checked')) {
-        jQuery(this).siblings('label').addClass('like-taxonomy-term');
-        jQuery(this).siblings('label').removeClass('unlike-taxonomy-term');
-      }
-      else {
-        jQuery(this).siblings('label').removeClass('like-taxonomy-term');
-        jQuery(this).siblings('label').addClass('unlike-taxonomy-term');
-      }
-    });
+Drupal.behaviors.chose_taxonomy = {
+  attach: function (context, settings) {
+    jQuery(document).ready(function() {
+      var clicked_image   = Drupal.settings.chose_taxonomy.cliked_image;
+      var unclicked_image = Drupal.settings.chose_taxonomy.uncliked_image;
+
+      jQuery('.field-widget-chose-taxonomy input').each(function() {
+        if (jQuery(this).is(':checked')) {
+          jQuery(this).siblings('label').addClass('like-taxonomy-term');
+          jQuery(this).siblings('label').removeClass('unlike-taxonomy-term');
+        }
+        else {
+          jQuery(this).siblings('label').removeClass('like-taxonomy-term');
+          jQuery(this).siblings('label').addClass('unlike-taxonomy-term');
+        }
+      });
+
+      jQuery('.unlike-taxonomy-term').css('background', 'url(/' + clicked_image + ') no-repeat scroll right center transparent');
+      jQuery('.like-taxonomy-term').css('background', 'url(/' + unclicked_image + ') no-repeat scroll right center transparent');
+
+      jQuery('.like-taxonomy-term').hover(function() {
+        jQuery(this).css('background', 'url(/' + clicked_image + ') no-repeat scroll right center transparent');
+      },function() {
+        jQuery(this).css('background', 'url(/' + unclicked_image + ') no-repeat scroll right center transparent');
+      });
+
+      jQuery('.unlike-taxonomy-term').hover(function() {
+        jQuery(this).css('background', 'url(/' + unclicked_image + ') no-repeat scroll right center transparent');
+      },function() {
+        jQuery(this).css('background', 'url(/' + clicked_image + ') no-repeat scroll right center transparent');
+      });
+
     });
   }};
 

3074ca82a0a30ef8703e0e214f6cfee9a607f3b2
Akash Jain <akashjain132@gmail.com>
2014-08-04 01:00:07 +0530

    change module name

--- js/autosave_taxonomy.js
+++ js/autosave_taxonomy.js
@@ -1,11 +1,11 @@
 (function (jQuery) {
-Drupal.behaviors.chose_taxonomy = {
+Drupal.behaviors.autosave_taxonomy = {
   attach: function (context, settings) {
     jQuery(document).ready(function() {
-      var clicked_image   = Drupal.settings.chose_taxonomy.cliked_image;
-      var unclicked_image = Drupal.settings.chose_taxonomy.uncliked_image;
+      var clicked_image   = Drupal.settings.autosave_taxonomy.cliked_image;
+      var unclicked_image = Drupal.settings.autosave_taxonomy.uncliked_image;
 
-      jQuery('.field-widget-chose-taxonomy input').each(function() {
+      jQuery('.field-widget-autosave-taxonomy input').each(function() {
         if (jQuery(this).is(':checked')) {
           jQuery(this).siblings('label').addClass('like-taxonomy-term');
           jQuery(this).siblings('label').removeClass('unlike-taxonomy-term');
